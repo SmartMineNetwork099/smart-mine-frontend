6ac2d71c9f19e01886f82f6589eaf52290878feb
zeeshan <zeeshandev099@gmail.com>
2025-08-03 10:29:03 +0500

    fix build errors

--- src/app/royaltyAndRewards/page.jsx
+++ src/app/royaltyAndRewards/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import RoyaltyAndRewards from '@/moduleS/royaltyAndRewards/index';
+import RoyaltyAndRewards from '@/modules/royaltyAndRewards/index';
 const page = () => {
     return (
         <>
