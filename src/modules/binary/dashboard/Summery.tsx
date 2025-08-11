d1c213fe5e88894096c21c18cc726cb022b6f56c
zeeshan <chzeeshanszforever099@gmail.com>
2025-08-11 13:27:31 +0500

    set routes

--- src/modules/binary/dashboard/Summery.tsx
+++ src/modules/binary/dashboard/Summery.tsx
@@ -8,101 +8,58 @@ import { RiTeamLine } from 'react-icons/ri';
 
 const summeryData: {
     name: string;
-    price: string;
+    price?: string;
+    number?: string;
     icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
 }[] = [
-        {
-            name: 'Total Bonus',
-            price: '0.001234 ST',
-            icon: IoGiftOutline,
-        },
-        {
-            name: 'Total Deposit',
-            price: '1.753849 ST',
-            icon: PiHandDeposit,
-        },
-        {
-            name: 'Direct Referrals',
-            price: '21',
-            icon: RiTeamLine,
-        },
-        {
-            name: 'My Community size',
-            price: '234',
-            icon: LiaUsersSolid,
-        },
-        {
-            name: 'Direct Bonus',
-            price: '2.001234 ST',
-            icon: IoGiftOutline,
-        },
-        {
-            name: 'Referral Bonus',
-            price: '9.345683 ST',
-            icon: IoGiftOutline,
-        },
-        {
-            name: 'Upgrade Bonus',
-            price: '3.001234 ST',
-            icon: IoGiftOutline,
-        },
-        {
-            name: 'Level Bonus',
-            price: '1.111234 ST',
-            icon: IoGiftOutline,
-        },
-        {
-            name: 'Royalty Bonus',
-            price: '10 ST',
-            icon: IoGiftOutline,
-        },
-        {
-            name: 'Reward Bonus',
-            price: '44.231234 ST',
-            icon: IoGiftOutline,
-        },
-    ];
+    { name: 'Total Bonus', price: '0.001234', icon: IoGiftOutline },
+    { name: 'Total Deposit', price: '1.753849', icon: PiHandDeposit },
+    { name: 'Direct Referrals', number: '0', icon: RiTeamLine },
+    { name: 'My Community size', number: '234', icon: LiaUsersSolid },
+    { name: 'Direct Bonus', price: '2.001234', icon: IoGiftOutline },
+    { name: 'Referral Bonus', price: '9.345683', icon: IoGiftOutline },
+    { name: 'Upgrade Bonus', price: '3.001234', icon: IoGiftOutline },
+    { name: 'Level Bonus', price: '0.0000', icon: IoGiftOutline },
+    { name: 'Royalty Bonus', price: '10', icon: IoGiftOutline },
+    { name: 'Reward Bonus', price: '44.231234', icon: IoGiftOutline },
+];
 
 const Summery = () => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-            {summeryData.map(
-                (
-                    item: {
-                        name: string;
-                        price: string;
-                        icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
-                    },
-                    index: number
-                ) => {
-                    const Icon = item.icon;
-                    return (
-                        <Card
-                            key={index}
-                            className="flex gap-2 items-center justify-between text-white px-3 sm:px-6 py-3"
-                        >
-                            <div>
-                                <p className="font-semibold text-sm sm:text-base mb-1 text-yellow-400">
-                                    {item.name}
-                                </p>
-                                <p className="font-semibold text-sm">
-                                    {item.price.includes('ST') ? (
-                                        <>
-                                            {item.price.replace(' ST', '')}{' '}
-                                            <span className="text-yellow-400">ST</span>
-                                        </>
-                                    ) : (
-                                        item.price
-                                    )}
-                                </p>
-                            </div>
-                            <div>
-                                <Icon className="text-2xl" />
-                            </div>
-                        </Card>
-                    );
-                }
-            )}
+            {summeryData.map((item, index) => {
+                const Icon = item.icon;
+
+                const value = item.price ?? item.number ?? '0';
+                const numericValue = parseFloat(value);
+                const valueColorClass =
+                    numericValue <= 0 ? 'text-red-500' : 'text-green-500';
+
+                return (
+                    <Card
+                        key={index}
+                        className="flex gap-2 items-center justify-between text-white px-3 sm:px-6 py-3"
+                    >
+                        <div>
+                            <p className="font-semibold text-sm sm:text-base mb-1 text-yellow-400">
+                                {item.name}
+                            </p>
+                            <p className={`font-semibold text-sm ${valueColorClass}`}>
+                                {item?.price && (
+                                    <>
+                                        {item.price}{' '}
+                                        <span className="text-yellow-400">ST</span>
+                                    </>
+                                )}
+                                {item?.number && <>{item?.number}</>}
+                            </p>
+                        </div>
+                        <div>
+                            <Icon className="text-2xl" />
+                        </div>
+                    </Card>
+                );
+            })}
         </div>
     );
 };
