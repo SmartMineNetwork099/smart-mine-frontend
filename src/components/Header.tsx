73c315757264f5179df14eaa9f7201f0cd18936f
zeeshan <chzeeshanszforever099@gmail.com>
2025-09-17 17:20:30 +0500

    Refactor Header component to include client directive and enhance logout functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import { useRouter } from 'next/navigation';
+"use client"
 import React from 'react'
+import { useRouter } from 'next/navigation';
 import { Button } from 'rizzui'
 import { DEFAULT_CURRENCY } from "@/constants/currency";
 import ROUTES from '@/constants/routes';
@@ -8,13 +9,17 @@ const Header = () => {
   const goHomePage = () => {
     router.push(ROUTES?.STACKING?.DASHBOARD);
   }
+  const handleLogout = () => {
+    ["walletData", "token"].forEach(key => localStorage.removeItem(key));
+    router.push(ROUTES?.AUTH?.LOGIN);
+  }
   return (
     <>
       <div className='flex items-center justify-between bg-black p-2'>
         <div onClick={goHomePage}>
           <p className='cursor-pointer font-bold border-2 border-yellow-300 text-yellow-300 py-2 px-3 rounded-full text-center'>{DEFAULT_CURRENCY}</p>
         </div>
-        <Button className='bg-red-500 px-3 py-2 cursor-pointer text-white font-semibold rounded-md border-0'>Logout</Button>
+        <Button onClick={handleLogout} className='bg-red-500 px-3 py-2 cursor-pointer text-white font-semibold rounded-md border-0'>Logout</Button>
       </div>
     </>
   )
