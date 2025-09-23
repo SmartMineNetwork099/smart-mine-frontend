a95588a9bfe02b1c3694a5da5524d722ed0ee4ea
zeeshan <chzeeshanszforever099@gmail.com>
2025-09-23 18:09:08 +0500

    working on issues

--- src/modules/stacking/NetworkOverview.tsx
+++ src/modules/stacking/NetworkOverview.tsx
@@ -35,14 +35,14 @@ const NetworkOverview = () => {
                 <p className='font-semibold sm:font-bold text-xl sm:text-3xl text-white'>
                     Network <span className='text-green-500'>Overview</span>
                 </p>
-                <div className='flex items-center justify-between sm:justify-start gap-2 mt-3 text-gray-300'>
-                    <div className='border border-green-500 rounded-lg p-2'>
-                        <p>Direct Team</p>
-                        <p className='text-sm sm:text-base'>{stats?.directTeam || 0}</p>
+                <div className='flex items-center justify-between sm:justify-start gap-2 mt-3 text-gray-300 text-sm sm:text-base'>
+                    <div className='border border-green-500 rounded-lg py-2 px-3'>
+                        <p className='font-semibold'>Direct Team</p>
+                        <p className=''>{stats?.directTeam || 0}</p>
                     </div>
-                    <div className='border border-green-500 rounded-lg p-2'>
-                        <p>Community Size</p>
-                        <p className='text-sm sm:text-base'>{stats?.communitySize || 0}</p>
+                    <div className='border border-green-500 rounded-lg py-2 px-3'>
+                        <p className='font-semibold'>Community Size</p>
+                        <p className=''>{stats?.communitySize || 0}</p>
                     </div>
                 </div>
             </Card>
