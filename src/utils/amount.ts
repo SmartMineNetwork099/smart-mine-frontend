723abb3c722ab1c6f484cf48886cc2e917acc635
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2026-03-06 20:40:24 +0500

    working on api integration in table view transactions

---
+++ src/utils/amount.ts
@@ -0,0 +1,5 @@
+export const formatTo4 = (value:any) => {
+  const num = Number(value || 0);
+  if (!Number.isFinite(num)) return "0.0000";
+  return (Math.floor(num * 10000) / 10000).toFixed(4);
+};
