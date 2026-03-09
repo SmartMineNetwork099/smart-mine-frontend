d30e7b3475fd760be5fc881ec49feb3b0e0670e9
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2026-03-09 12:07:23 +0500

    txhash validation

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const skipRoutes = ["/auth/login"];
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const token = request.cookies.get("accessToken")?.value;
+
+  // Next.js internals aur static files skip
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/favicon.ico") ||
+    /\.(png|jpg|jpeg|svg|gif|webp|ico|css|js|map)$/.test(pathname)
+  ) {
+    return NextResponse.next();
+  }
+
+  const isSkipRoute = skipRoutes.includes(pathname);
+
+  // user login nahi hai
+  if (!token && !isSkipRoute) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  // user login hai aur login page par aa gaya
+  if (token && pathname === "/auth/login") {
+    return NextResponse.redirect(new URL("/stacking/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/:path*"],
+};
