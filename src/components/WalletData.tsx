e8af8033486f14e15a2fc7629e22a11185b9daad
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2026-03-20 22:52:49 +0500

    code update

--- src/components/WalletData.tsx
+++ src/components/WalletData.tsx
@@ -150,8 +150,8 @@ const WalletData = () => {
   const walletInfo = [
     { name: "Today Income", transactions: `${formatAmount(walletData?.wallet?.todayIncome || 0)} $` },
     { name: "Total Income", transactions: `${formatAmount(walletData?.wallet?.totalEarning || 0)} $` },
-    // { name: "Pending Income", transactions: `${formatAmount(walletData?.wallet?.pendingBonus || 0)} $` },
-    // { name: "Collectable Income", transactions: `${formatAmount(walletData?.wallet?.collectableBonus || 0)} $` },
+    { name: "Pending Income", transactions: `${formatAmount(walletData?.wallet?.pendingBonus || 0)} $` },
+    { name: "Collectable Income", transactions: `${formatAmount(walletData?.wallet?.collectableBonus || 0)} $` },
     { name: "Total Withdraw", transactions: `${formatAmount(walletData?.wallet?.totalWithdraw || 0)} $` },
     { name: "Share Income", transactions: `${formatAmount(walletData?.wallet?.shareIncome || 0)} $` },
     { name: "YesterDay Loss", transactions: `${formatAmount(walletData?.wallet?.yesterdayLossIncome || 0)} $` },
