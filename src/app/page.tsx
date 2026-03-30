cb02d1fd40d7bfe1ac98674f220951af602b55cb
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2026-03-30 20:00:59 +0500

    fix: homepage redirect to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
-
 "use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import ROUTES from "@/constants/routes";
 import { getAccessToken } from "@/utils/authSession";
-export default function Home() {
-    const router = useRouter();
 
-    useEffect(() => {
-        const accessToken = getAccessToken();
-        if (accessToken) {
-            router.replace(ROUTES?.STACKING?.DASHBOARD);
-        }
-    }, [router]);
-    return null;
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const accessToken = getAccessToken();
+
+    if (accessToken) {
+      router.replace(ROUTES?.STACKING?.DASHBOARD);
+    } else {
+      router.replace(ROUTES?.AUTH?.LOGIN);
+    }
+  }, [router]);
+
+  return null;
 }
