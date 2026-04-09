0631d9fa40af1e925c638918993d5681835052f7
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2026-04-09 06:58:33 +0500

    fix table width stacking plan

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ const Header = () => {
         <div onClick={goHomePage} className='flex items-center gap-3 pl-1'>
           {/* <p className='cursor-pointer font-bold border-2 border-yellow-300 text-yellow-300 py-2 px-3 rounded-full text-center'>{DEFAULT_CURRENCY}</p> */}
           <Image src='/logo.JPEG' width={40} height={40} alt='company logo'/>
-          <p className='text-green-500 font-bold text-3xl sm:text-4xl'>Rise Star</p>
+          <p className='text-gray-300 font-bold text-3xl sm:text-4xl'>Rise Star</p>
         </div>
         
         <Button onClick={handleLogout} className='bg-red-500 px-3 py-2 cursor-pointer text-white font-semibold rounded-md border-0'>Logout</Button>
